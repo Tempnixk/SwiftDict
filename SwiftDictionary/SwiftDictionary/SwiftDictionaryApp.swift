f4c7ec3e26342c27cbfbe7a4c02e2c4008820d90
uudquark <chm49@naver.com>
2022-04-28 16:25:18 +0900

    Search Bar IMPLEMENTED
    Search Bar implemented

---
+++ SwiftDictionary/SwiftDictionary/SwiftDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDictionaryApp.swift
+//  SwiftDictionary
+//
+//  Created by Tempnixk on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
